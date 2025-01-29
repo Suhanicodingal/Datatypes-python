7b8a3c8d10c166905fc7454c2104cfe0b1383790
Suhanicodingal <amritasrivastava849@gmail.com>
2025-01-29 15:45:35 +0530

    Add files via upload

---
+++ Congratulations.py
@@ -0,0 +1,14 @@
+a="CONGRATULATIONS"
+print("type of a: ",type(a))
+
+b="For winning GOLD MEDAL in state games"
+print("type of b: ", type(b))
+
+c="Congratulations again"
+print("type of c: ", type(c))
+
+d="keep winning medals"
+print("type of d: ", type(d))
+
+e="CONGRATS for your performance"
+print("type of e: ",type(e))
