dba07d1f921c480b9ace80c0fadbfe4f9a0b2d3c
Suhanicodingal <amritasrivastava849@gmail.com>
2025-01-29 13:16:12 +0530

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,12 @@
+name = "penguin"
+age= 15
+is_student =True
+weight =38.5
+
+print("\n After Type casting.....")
+age=str(age)
+print(age)
+print("Data type of age is", type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of Weight is",type(weight))
